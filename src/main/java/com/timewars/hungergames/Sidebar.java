824d8527fdba4601317286b5ca858067c1c97aa8
KLFzx <kliffa7@gmail.com>
2021-08-21 21:55:28 +0100

    Removed NetherBoard

--- src/main/java/com/timewars/hungergames/Sidebar.java
+++ src/main/java/com/timewars/hungergames/Sidebar.java
@@ -1,11 +1,12 @@
 package com.timewars.hungergames;
 
-import fr.minuskube.netherboard.Netherboard;
-import fr.minuskube.netherboard.bukkit.BPlayerBoard;
+//import fr.minuskube.netherboard.Netherboard;
+//import fr.minuskube.netherboard.bukkit.BPlayerBoard;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.*;
 
 public class Sidebar {
 
@@ -17,21 +18,57 @@ public class Sidebar {
     }
 
     public void createBoard(Player player) {
-        BPlayerBoard board = Netherboard.instance().createBoard(player,
+//        BPlayerBoard board = Netherboard.instance().createBoard(player,
+//                ChatColor.AQUA.toString() + ChatColor.ITALIC + "TimeWars");
+//
+//
+//        board.setAll(
+//                ChatColor.YELLOW + ChatColor.BOLD.toString() + "Players"  ,
+//                Bukkit.getOnlinePlayers().size() + " alive",
+//                "   ",
+//                ChatColor.GOLD + ChatColor.BOLD.toString() + "Stats",
+//                "Kills: " + ChatColor.GREEN + player.getStatistic(Statistic.PLAYER_KILLS),
+//                "  " ,
+//                ChatColor.YELLOW + ChatColor.BOLD.toString() + "Time: " +
+//                        ChatColor.RESET +
+//                        (getGameTime())
+//        );
+
+
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+
+        Objective obj = board.registerNewObjective("HubScoreboard-1", "dummy",
                 ChatColor.AQUA.toString() + ChatColor.ITALIC + "TimeWars");
 
-        board.setAll(
-                ChatColor.YELLOW + ChatColor.BOLD.toString() + "Players"  ,
-                Bukkit.getOnlinePlayers().size() + " alive",
-                "   ",
-                ChatColor.GOLD + ChatColor.BOLD.toString() + "Stats",
-                "Kills: " + ChatColor.GREEN + player.getStatistic(Statistic.PLAYER_KILLS),
-                "  " ,
-                ChatColor.YELLOW + ChatColor.BOLD.toString() + "Time: " +
-                        ChatColor.RESET +
-                        (getGameTime())
-        );
+        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
 
+        Score score = obj.getScore(ChatColor.BLUE + "----------------");
+        score.setScore(7);
+
+        Score score1 = obj.getScore(ChatColor.YELLOW + ChatColor.BOLD.toString() + "Players");
+        score1.setScore(6);
+
+        Score score2 = obj.getScore(Bukkit.getOnlinePlayers().size() + " alive(NO)");
+        score2.setScore(5); //TODO change this to another way
+
+        Score score3 = obj.getScore("   ");
+        score3.setScore(4);
+
+        Score score4 = obj.getScore(ChatColor.GOLD + ChatColor.BOLD.toString() + "Stats");
+        score4.setScore(3);
+
+        Score score5 = obj.getScore("Kills: " + ChatColor.GREEN + player.getStatistic(Statistic.PLAYER_KILLS));
+        score5.setScore(2); //TODO change this to another way
+
+        Score score6 = obj.getScore("  " );
+        score6.setScore(1);
+
+        Score score7 = obj.getScore(ChatColor.YELLOW + ChatColor.BOLD.toString() + "Time: " +
+                        ChatColor.RESET + (getGameTime()));
+        score7.setScore(0);
+
+        player.setScoreboard(board);
     }
 
     public String getGameTime()
